e8d93e37131b805aad58c021645af595d0de769e
Fredrik Karlsson <k.fredrik.karlsson@gmail.com>
2022-08-05 16:47:21 +0200

    Add files via upload

--- website/auth.py
+++ website/auth.py
@@ -1,12 +1,9 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from .models import User
-from werkzeug.security import generate_password_hash, check_password_hash
-from . import db
 from flask_login import login_user, login_required, logout_user, current_user
-import base64
-from covid19dh import covid19
-from io import BytesIO
-from matplotlib.figure import Figure
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from . import db
+from .models import User
 
 auth = Blueprint('auth', __name__)
 
@@ -71,43 +68,4 @@ def sign_up():
     return render_template("sign_up.html", user=current_user)
 
 
-@auth.route('/work', methods=['GET', 'POST'])
-def plot_work():
-    if request.method == 'POST':
-        country = request.form.get('country')
-        parameter = request.form.get('parameter')
-        data, src = covid19(country)
 
-        if data is not None:
-            x = data.date
-
-            if parameter == "deaths":
-                y = data.deaths
-            elif parameter == "confirmed":
-                y = data.confirmed
-            elif parameter == "people vaccinated":
-                y = data.people_vaccinated
-            elif parameter == "icu":
-                y = data.icu
-            else:
-                y = data.recovered
-
-            fig = Figure()
-            ax = fig.subplots()
-            ax.plot(x, y)
-            # Save it to a temporary buffer.
-            buf = BytesIO()
-            fig.savefig(buf, format="png")
-            # Embed the result in the html output.
-            view = base64.b64encode(buf.getbuffer()).decode("ascii")
-            return f"<div><table width='100%' height='100%' align='center' valign='center'><center><img src='data:image/png;base64,{view}' alt = 'foo' /></center></div>"
-
-
-        else:
-            flash("Invalid country id", category="error")
-    return render_template("work.html", user=current_user)
-
-
-@auth.route('who_am_I', methods=['GET', 'POST'])
-def who_am_I():
-    return render_template('who_am_I.html', user=current_user)
--- website/view.py
+++ website/view.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
-from .models import Note
-from . import db
-import json
+
+import base64
+from covid19dh import covid19
+from matplotlib.figure import Figure
+from io import BytesIO
+
 
 view = Blueprint('view', __name__)
 
@@ -11,27 +14,46 @@ view = Blueprint('view', __name__)
 @login_required
 def home():
     if request.method == 'POST':
-        note = request.form.get('note')
+        country = request.form.get('country')
+        parameter = request.form.get('parameter')
+        data, src = covid19(country)
 
-        if len(note) < 1:
-            flash('Note is too short', category='error')
+        if data is not None:
+            x = data.date
+
+            if parameter == "deaths":
+                y = data.deaths
+
+            elif parameter == "confirmed":
+                y = data.confirmed
+            elif parameter == "people vaccinated":
+                y = data.people_vaccinated
+            elif parameter == "icu":
+                y = data.icu
+            else:
+                y = data.recovered
+
+            if y.isnull().all():
+                flash("Data is not available", category="error")
+                return render_template("work.html", user=current_user)
+            print(y.isnull().all(), y)
+
+            fig = Figure()
+            ax = fig.subplots()
+            ax.plot(x, y)
+
+            # Save it to a temporary buffer.
+            buf = BytesIO()
+            fig.savefig(buf, format="png")
+            # Embed the result in the html output.
+            figure = base64.b64encode(buf.getbuffer()).decode("ascii")
+            return f"<div><table width='100%' height='100%' align='center' valign='center'><center><img src='data:image/png;base64,{figure}' alt = 'foo' /></center></div>"
+
+
         else:
-            new_note = Note(data=note, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
-            flash('Note added', category="success")
-
-    return render_template("home.html", user=current_user)
+            flash("Invalid country id", category="error")
+    return render_template("work.html", user=current_user)
 
 
-@view.route('/delete-note', methods=['POST'])
-def delete_note():
-    note = json.loads(request.data)
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
 
-    return jsonify({})
+
